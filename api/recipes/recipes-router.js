ed47da44a695b1c3cdadf9395aac9e9d3a95d3de
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-10 10:25:35 -0500

    started building out recipes-router

---
+++ api/recipes/recipes-router.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        customMessage: 'problem occured inside the recipes router',
+        message: err.message,
+        stack: err.stack
+    })
+});
+
+module.exports = router;
