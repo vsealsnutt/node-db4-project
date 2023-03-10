9a77250ea6e349dd6c297ca4d46ddb377e75c4cf
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-10 10:56:49 -0500

    wrote an endpoint to fetch a recipe by its id

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
+const Recipe = require('./recipes-model');
 
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+        .then(recipe => {
+            res.status(200).json(recipe)
+        })
+        .catch(next)
+})
 
 router.use((err, req, res, next) => { //eslint-disable-line
     res.status(500).json({
